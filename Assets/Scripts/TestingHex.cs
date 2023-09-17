a97e0c5d9ce067a5e772b0fa396960ae7bb03685
Kaynecoirir <arseniy.grodno@mail.ru>
2023-09-17 22:27:15 +0300

    attack sys

--- Assets/Scripts/TestingHex.cs
+++ Assets/Scripts/TestingHex.cs
@@ -48,10 +48,35 @@ public class TestingHex : MonoBehaviour
 		if(Input.GetMouseButtonDown(0))
 		{
 			Vector3 pos = DebugUtilites.GetMouseWorldPosition();
+			int x, y, seq;
+			PathfindingHex.Instance.GetGrid().GetXY(pos, out x, out y, out seq);
 
+			if (currentSoldier != null && soldierArray[y, x] != null && (currentSoldier.indexY != y || currentSoldier.indexX != x))
+			{
+				SoldierData targetSoldier = soldierArray[y, x];
+				if (PathfindingHex.Instance.GetGrid().isVertical)
+				{
+					int tx = 0, ty = 0;
+					if (seq == 0) { tx = x - 1 + y % 2; ty = y + 1; }
+					else if (seq == 1) { tx = x - 1; ty = y; }
+					else if (seq == 2) { tx = x - 1 + y % 2; ty = y - 1; }
+					else if (seq == 3) { tx = x + y % 2; ty = y - 1; }
+					else if (seq == 4) { tx = x + 1; ty = y; }
+					else if (seq == 5) { tx = x + y % 2; ty = y + 1; }
+					if (currentSoldier.moveControll.GoToPosition(tx, ty) || (currentSoldier.indexY == ty && currentSoldier.indexX == tx)) 
+					{
+						if (targetSoldier != null) currentSoldier.Attack(targetSoldier);
+						NextSoldier(); 
+					}
+				}
+				else
+				{
+
+				}
+			}
 			if (currentSoldier != null)
 			{
-				
+
 				if (PathfindingHex.Instance.GetGrid().GetValue(pos).Value.IsWalking && currentSoldier.moveControll.GoToPosition(DebugUtilites.GetMouseWorldPosition()))
 				{
 
