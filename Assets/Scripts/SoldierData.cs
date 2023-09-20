d1252ef4bdfe07ee9b4b4e14ef6d26a7a99e3daf
Kaynecoirir <arseniy.grodno@mail.ru>
2023-09-20 17:17:10 +0300

    20.09.2023

--- Assets/Scripts/SoldierData.cs
+++ Assets/Scripts/SoldierData.cs
@@ -31,6 +31,7 @@ public class SoldierData : MonoBehaviour
         {
             Debug.Log($"{this.name} Attack {targetSoldier.name} on {attackPower * count}\nNow Army[{targetSoldier.count}] HP[{targetSoldier.curHealth} / {targetSoldier.maxHealth}]");
         };
+        // I have problem. Ok? I can't dream project and write code...
     }
 
     void Update()
